df1f2f9ac73f99b9c40937c64b1434874682698a
Francisco Pereira <65984151+FranciscoPereira987@users.noreply.github.com>
2024-09-06 15:55:30 +0000

    ADD: Script parsing, without functionality

--- bitcoinlib/script.go
+++ bitcoinlib/script.go
@@ -2,15 +2,15 @@ package bitcoinlib
 
 import "io"
 
-type Script struct {}
+type Script struct{}
 
 func ParseScript(from io.Reader) []byte {
 	last_four := make([]byte, 4)
 	new_four := make([]byte, 4)
 	val, _ := from.Read(new_four)
-	for  val >= 4 {
+	for val >= 4 {
 		copy(last_four, new_four)
 	}
-	copy(last_four[3-val:], new_four[:3-val])
+	copy(last_four[3-val:], new_four[:val])
 	return last_four
 }
